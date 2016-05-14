eff079cdc8f297ead62fee26aeab93cb716a38fc
mqm <762577099@qq.com>
2016-05-14 18:37:07 +0800

    aa

---
+++ src/main/java/com/mqm/base/dao/HibernateDao.java
@@ -0,0 +1,19 @@
+package com.mqm.base.dao;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public abstract class HibernateDao {
+
+	private SessionFactory sessionFactory;
+
+    @Autowired
+    public void setSessionFactory(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
+
+    public Session getSession() {
+        return sessionFactory.getCurrentSession();
+    }  
+}
