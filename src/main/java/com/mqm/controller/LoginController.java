eae496d2844be15e52b04cc38aaef625ad1cb67d
mengqingmeng <762577099@qq.com>
2016-07-19 23:37:57 +0800

    shiro

--- src/main/java/com/mqm/controller/LoginController.java
+++ src/main/java/com/mqm/controller/LoginController.java
@@ -3,65 +3,81 @@ package com.mqm.controller;
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authc.AuthenticationException;
 import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.crypto.hash.Sha256Hash;
 import org.apache.shiro.subject.Subject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.mqm.entity.User;
 import com.mqm.service.UserServiceI;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Controller
 @RequestMapping("web")
 public class LoginController {
+	Logger logger = LoggerFactory.getLogger(getClass());
+	
 	@Autowired UserServiceI userService;
 	
 	@RequestMapping("/toLogin")
 	public String loginGet(){
+		logger.debug("进入登录页面");
 		return "/web/login/login";
 	}
+
+	@RequestMapping("/admin")
+	public String admin(){
+		logger.debug("进入admin页面");
+		return "/web/login/admin";
+	}
 	
-	@RequestMapping(value = "/loginPost",method = RequestMethod.POST)
+	@RequestMapping(value = "loginPost",method = RequestMethod.POST)
 	public String loginPost(@RequestParam("username") String username,@RequestParam("password") String password){
         SecurityUtils.getSecurityManager().logout(SecurityUtils.getSubject());  
-        // 登录后存放进shiro token  
-        UsernamePasswordToken token = new UsernamePasswordToken(username, password);  
+        // 登录后存放进shiro token 
+        String repassword = new Sha256Hash(password).toHex();
+        UsernamePasswordToken token = new UsernamePasswordToken(username, repassword);  
         Subject subject = SecurityUtils.getSubject();  
-       
         try {
-        	 subject.login(token);  
+        	 subject.login(token);
+        	 logger.debug("登录成功,用户:"+username);
 		    }
 		    catch (AuthenticationException ae) {
-		    	System.out.println("登录失败:"+ae.getMessage());
-		    	return "/web/login/error";
+		    	logger.debug("登录失败:"+ae.getMessage());
+		    	return "/error/error";
 		    }
 		return "/web/login/loginSuccess";
 	}
 	
-	@RequestMapping("/toRegister")
-	public String admin(){
+	@RequestMapping(value = "/toRegister")
+	public String toRegister(Model model){
 		return "/web/register/register";
 	}
 	
 	@RequestMapping(value = "/registerPost",method = RequestMethod.POST)
-	public String registerPost(@RequestParam("username") String username,@RequestParam("password") String password){
+	public String registerPost(@RequestParam("username") String username,@RequestParam("password") String password,ModelMap model){
         SecurityUtils.getSecurityManager().logout(SecurityUtils.getSubject());  
-        // 登录后存放进shiro token  
-        UsernamePasswordToken token = new UsernamePasswordToken(username, password);  
-        Subject subject = SecurityUtils.getSubject(); 
-       
-        userService.createUser(username, password);
-        try {
-        	 subject.login(token);  
-		    }
-		    catch (AuthenticationException ae) {
-		    	System.out.println("登录失败:"+ae.getMessage());
-		    	return "/web/login/error";
-		    }
-		return "/web/login/loginSuccess";
+        model.addAttribute("user", new User());
+        User user = userService.findUser(username);
+        
+        if(user == null){
+        	userService.createUser(username, password);
+        	// 登录后存放进shiro token  
+        	String repassword = new Sha256Hash(password).toHex();
+        	UsernamePasswordToken token = new UsernamePasswordToken(username, repassword);  
+        	Subject subject = SecurityUtils.getSubject(); 
+        	subject.login(token);
+        	
+        	return "/web/login/loginSuccess";
+        }else{
+        	model.put("errMsg", "用户名已存在");
+        	return "/error/error";
+        }
 	}
-	
 }
--- src/main/java/com/mqm/security/MyShiroRealm.java
+++ src/main/java/com/mqm/security/MyShiroRealm.java
@@ -1,12 +1,24 @@
 package com.mqm.security;
 
+import org.apache.commons.lang.builder.ReflectionToStringBuilder;
+
+import org.apache.commons.lang.builder.ToStringStyle;
 import org.apache.shiro.authc.AuthenticationException;
 import org.apache.shiro.authc.AuthenticationInfo;
 import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.authc.SimpleAuthenticationInfo;
 import org.apache.shiro.authz.AuthorizationInfo;
+import org.apache.shiro.authz.SimpleAuthorizationInfo;
 import org.apache.shiro.realm.AuthorizingRealm;
 import org.apache.shiro.subject.PrincipalCollection;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import com.mqm.entity.Role;
+import com.mqm.entity.User;
+import com.mqm.service.UserServiceI;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /****
  * 自定义Realm
@@ -14,22 +26,27 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class MyShiroRealm extends AuthorizingRealm {
+	@Autowired UserServiceI userService; 
+	
+	protected Logger logger = LoggerFactory.getLogger(getClass());
 	/**
-	 * 认证
+	 * 认证，验证用户名和密码
 	 */
     protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken authcToken) throws AuthenticationException {
+    	
+    	logger.debug("验证当前Subject时获取到token为" + ReflectionToStringBuilder.toString(authcToken, ToStringStyle.MULTI_LINE_STYLE));  
     	//登录信息
     	//1.从token中获取登录的用户名，并不获取密码
-    	
+    	String username = authcToken.getPrincipal().toString();
     	//2.通过用户名，从数据库查询用户信息
-    	
+    	User user = userService.findUser(username);
     	//3.创建SimpleAuthenticationInfo对象，并返回，密码校验由shiro进行
-    	
+    	SimpleAuthenticationInfo info = new SimpleAuthenticationInfo(user.getUserName(),user.getPassword(),getName());
     	//4.密码加密
     	//1)选择加密方式
     	//2)设置盐值：一般从数据库中得到
     	//认证信息
-    	return null;
+    	return info;
     }
     
     // @PostConstruct 相当于bean节点的 init-method 配置
@@ -41,10 +58,22 @@ public class MyShiroRealm extends AuthorizingRealm {
 //    }
     
     /**
-     * 授权
+     * 授权，权限管理
      */
     protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principals) {
-    	return null;
+    	String userName = (String)principals.fromRealm(getName()).iterator().next();
+    	User user = userService.findUser(userName);
+    	if( user != null ) {
+            SimpleAuthorizationInfo info = new SimpleAuthorizationInfo();
+            for( Role role : user.getRoles()) {
+            	logger.debug("role:"+role.getName()+";permission:"+role.getName());
+                info.addRole(role.getName());
+                info.addStringPermission( role.getName() );
+            }
+            return info;
+        } else {
+            return null;
+        }
     }
 
 }
